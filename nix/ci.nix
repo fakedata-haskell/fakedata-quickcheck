f4a2f269354173d5e03561d5062035e103a3d10e
Jappie Klooster <jappieklooster@hotmail.com>
2020-10-13 11:26:06 +0200

    Disable 865

--- nix/ci.nix
+++ nix/ci.nix
@@ -3,7 +3,7 @@
 }:
 {
   ghc-def = import ../default.nix { inherit pkgs; };
-  ghc-865 = import ../default.nix {compiler = "ghc865"; inherit pkgs; };
+  # ghc-865 = import ../default.nix {compiler = "ghc865"; inherit pkgs; }; # TODO can't find cabal?
   ghc-883 = import ../default.nix {compiler = "ghc883"; inherit pkgs;};
   ghc-810 = import ../default.nix {compiler = "ghc8101"; inherit pkgs;};
   # test = pkgs.nixosTest nix/test.nix;
