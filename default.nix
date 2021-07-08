5fc2b9cb3037a930136a28d86efdfd17d7562473
Jappie Klooster <jappieklooster@hotmail.com>
2021-07-08 14:16:11 +0200

    Re-enable nix based ci check
    Upgrade workflow stuff in cachix
    
    Disable cache
    
    It's quite fast building from scratch anyway
    
    Fix original ci.
    
    Fix merge issues
    
    Reset cabal file based on master

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
-{ pkgs ? import ./nix/pin.nix { },
-  compiler ? "ghc884",
+{ pkgs ? import ./nix/pin.nix {},
   ... }:
 
 let
-  hpkgs = pkgs.haskell.packages.${compiler};
+  hpkgs = pkgs.haskellPackages;
   ignore = import ./nix/gitignoreSource.nix { inherit (pkgs) lib; };
   # https://github.com/NixOS/nixpkgs/blob/dbacb52ad8/pkgs/development/haskell-modules/make-package-set.nix#L216
   src = ignore.gitignoreSource ./.;
   cabal2nix =
     hpkgs.callCabal2nix "fakedata-quickcheck" src {
+
+      fakedata = pkgs.haskell.lib.unmarkBroken
+        (pkgs.haskell.lib.dontCheck hpkgs.fakedata);
     };
 in
 # https://github.com/NixOS/nixpkgs/blob/dbacb52ad8/pkgs/development/haskell-modules/generic-builder.nix#L13
